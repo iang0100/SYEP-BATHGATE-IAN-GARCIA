dc0ab91ef2ecb186040aa40b6c2f4f4e1c108d13
iang0100 <Iangfavio391@gmail.com>
2025-08-11 01:05:13 +0000

    Testimonials

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const reviews = [
   {
 
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
-    img: 'https://www.course-api.com/images/people/person-1.jpeg',
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?",
+    name: 'Alain Latortue',
+    job: 'Teacher',
+    img: 'https://media.licdn.com/dms/image/v2/C4D03AQH1hb_YiDgvkQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1662519432541?e=1757548800&v=beta&t=dWbGu_R7hN520baUk1mb3oWycDN6pFQmicuewjc2cbc',
+    text: "I really enjoyed teaching you and thought that you gave some insightful thoughts and words during our class discussions.",
   },
   {
     id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
-    img: 'https://www.course-api.com/images/people/person-2.jpeg',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
+    name: 'Emma Garcia',
+    job: 'Geometry Teacher',
+    img: 'https://images.squarespace-cdn.com/content/v1/5bec73fa3917eec31d42357d/1628120571538-GLX0BDTX4GWXZ8SEO3MU/Screen+Shot+2021-08-04+at+7.41.13+PM.png?format=1000w',
+    text: 'Ian, you always finish your assignments on time, which shows how responsible and hardworking you are. You hold your own and show maturity beyond your years.',
   },
   {
     id: 3,
-    name: 'peter jones',
+    name: 'Eve Abraha',
     job: 'intern',
-    img: 'https://www.course-api.com/images/people/person-4.jpeg',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
+    img: 'https://images.squarespace-cdn.com/content/v1/5bec73fa3917eec31d42357d/1628120707534-L3T1VRB7DRMX8IQNQ3SY/Screen+Shot+2021-08-04+at+7.41.40+PM.png?format=1000w',
+    text: 'Ian, you are such a kind and lovely student. You are very intelligent and when you buckle down, you are able to produce great work. I am very proud of you because you really came out of your shell this school year! Great job!',
   },
 ];
 // select items
